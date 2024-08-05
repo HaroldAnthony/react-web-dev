992577508c89b8f84b43c4488eefef4a2e3fe246
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-08-05 13:53:52 +0530

    react map fn done

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,6 +1,23 @@
 import React from "react";
 
 const App = () => {
+
+
+
+  var a = [4,3,6,5,7];
+  
+  //STANDARD METHOD
+  // var b = a.map( function mapping(x){
+  //   return x*2;
+  // });
+
+  //ARROW FUNCTION METHOD
+
+  const b = a.map((x) => x*2)
+  console.log(b);
+
+
+
   return <div>App</div>;
 };
 
