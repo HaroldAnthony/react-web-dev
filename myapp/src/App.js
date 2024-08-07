1b790221eda2b10247dded2d1e56e8ffefa72dbd
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-08-07 11:40:24 +0530

    reduce acc curr done

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -6,12 +6,11 @@ const App = () => {
 
   var a = [4,3,6,5,7];
 
-  // var b = a.filter((x) => x>3);
-  // console.log(b);
+  var b = a.reduce((acc, curr) => {
+    //return acc;  - acc is the initial/first value of the array
+    //return curr; - curr is the final/last value of the array
+  });
 
-  //COMBINING MAP AND FILTER METH
-
-  var b = a.map((x) => x/2).filter((x) => x>2);
   console.log(b);
 
 
