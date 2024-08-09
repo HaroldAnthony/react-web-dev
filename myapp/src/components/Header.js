4dda0e41bb7d17076b8cf0942df60b0cd5c568b6
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-08-09 11:27:34 +0530

    fc useState 09-08-24

--- myapp/src/components/Header.js
+++ myapp/src/components/Header.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React from "react";
 import styles from "./header.module.css";
+import { useState } from "react";
 
 const Header = () => {
-  return <div className={styles.header}>Header</div>
-}
+  const [value, setvalue] = useState(0);
+
+  const valuehandler = () => {
+    setvalue(value+1);
+    console.log(value);
+  }
+
+  return (
+    <div className={styles.header}>
+      <p>This is a paragraph</p>
+      <button onClick={valuehandler}>Click here</button>
+    </div>
+  );
+};
 
 export default Header;
