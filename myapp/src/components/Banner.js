a5e623e9906f91636ebfeb50ffdc0540dce5eea2
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-08-12 11:01:27 +0530

    basic props used

--- myapp/src/components/Banner.js
+++ myapp/src/components/Banner.js
@@ -1,8 +1,9 @@
 import React from "react";
 
-const Banner = () => {
+const Banner = (props) => {
   return (
     <div>
+        <p>{props.heading}</p>
       Banner
       <button>Click here</button>
     </div>
