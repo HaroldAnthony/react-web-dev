9e8d35d43fb93baee57e6b887f696356a3ce13d6
Harold Anthony <113414190+HaroldAnthony@users.noreply.github.com>
2024-08-13 12:18:10 +0530

    react concepts done

--- myapp/src/components/Header.js
+++ myapp/src/components/Header.js
@@ -1,15 +1,24 @@
 import React from "react";
+import styles from "./header.module.css";
 import { useState, useEffect} from "react";
 
 const Header = () => {
   const [value, setvalue] = useState(0);
+  const [statement , setstatement ] = useState(true);
 
   const valuehandler = () => {
+    // setvalue(value+1);
+    // console.log(value);
     setvalue(value + 1);
   };
 
   const subtracthandler = () => {
     setvalue(value - 1);
+  };
+
+  const statementhandler = () => {
+    setstatement(!statement);
+    console.log(statement);
   };
 
   useEffect(() => {
@@ -18,9 +27,10 @@ const Header = () => {
 
 
   return (                                                              
-    <div>
+    <div className={styles.header}>
       <button onClick={valuehandler}>Addition</button>
       <button onClick={subtracthandler}>Subtraction</button>
+      <button onClick={statementhandler}>Boolean</button>
       <h1>{value}</h1> 
     </div>
   );
